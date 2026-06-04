c51afece41419d258de79806c681f2a01510ea15
Pratik Kumar <pratikkumarsbg@gmail.com>
2026-06-04 23:43:20 +0530

    added product application

---
+++ projects/products/webpack.config.js
@@ -0,0 +1,16 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'products',
+
+  exposes: {
+    './Component': './projects/products/src/app/app.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+});
